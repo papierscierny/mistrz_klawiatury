ab211fcf5a2745f7b284a1746c693f74327367aa
papier ścierny <papierscierny5@gmail.com>
2025-05-31 10:32:06 +0200

    Wybór trudności Poprawa

--- Project/test/Mistrz_klawiatury_test.py
+++ Project/test/Mistrz_klawiatury_test.py
@@ -4,10 +4,5 @@ import unittest
 
 from Mistrz_Klawiatury import *
 
-class GetUniqueLettersTest(unittest.TestCase):
-    def test_less_than_3_unique_letters(self):
-        self.assertSetEqual(set(), get_unique_letters('ah'))
 
-    def test_at_least_3_unique_letters(self):
-        self.assertSetEqual({'k', 'a', 'y'}, get_unique_letters('kayak'))
 
